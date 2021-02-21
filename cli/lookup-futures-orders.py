a060a39bfb18afa931d08bd6e9d717afff319679
yuez-gdm <yue.zhao@gdmexchange.io>
2021-02-20 22:10:52 -0500

    fix

---
+++ cli/lookup-futures-orders.py
@@ -0,0 +1,40 @@
+import os
+import click
+import requests
+from pprint import pprint
+
+# Local imports
+from util import *
+
+
+@click.command()
+@click.option("--config", type=str, default=None, help="path to the config file")
+@click.option("--botname", type=click.Choice(["bitmax", "bitmax-sandbox"]), default="bitmax-sandbox", help="specify the bot to use")
+@click.option("--order-id", type=str, required=True, help="a single order Id, or multiple orderId separated by comma")
+@click.option('--verbose/--no-verbose', default=False)
+def run(config, botname, order_id, verbose):
+
+    cfg = load_config(get_config_or_default(config), botname)
+
+    host = cfg['base-url']
+    group = cfg['group']
+    apikey = cfg['apikey']
+    secret = cfg['secret']
+
+    path = "v2/futures/order/status"
+    url = f"{host}/api/pro/" + path
+
+    ts = utc_timestamp()
+
+    if verbose:
+        print(f"url: {url}")
+
+    headers = make_auth_headers(ts, path, apikey, secret)
+    res = requests.get(url, headers=headers, params=dict(orderId=order_id))
+
+    data = parse_response(res)
+    print(json.dumps(data, indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    run()
---
+++ cli/place-order-batch.py
@@ -0,0 +1,68 @@
+import os
+import click
+import requests
+from pprint import pprint
+
+# Local imports 
+from util import *
+
+
+@click.command()
+@click.option("--config", type=str, default=None, help="path to the config file")
+@click.option("--botname", type=click.Choice(["bitmax", "bitmax-sandbox"]), default="bitmax-sandbox", help="specify the bot to use")
+@click.option("--symbol", type=str, default='BTC-PERP')
+@click.option("--price", type=str, default='30000')
+@click.option("--qty", type=str, default='0.1')
+@click.option("--order-type", type=str, default="limit")
+@click.option("--side", type=click.Choice(['buy', 'sell']), default='buy')
+@click.option('--verbose/--no-verbose', default=False)
+def run(config, botname, symbol, price, qty, order_type, side, verbose):
+    
+    cfg = load_config(get_config_or_default(config), "bitmax-sandbox")
+
+    host = cfg['base-url']
+    group = cfg['group']
+    apikey = cfg['apikey']
+    secret = cfg['secret']
+
+    path = "v2/futures/order/batch"
+    url = f"{host}/{group}/api/pro/" + path
+
+    ts = utc_timestamp()
+
+    vs = symbol.split(',')
+    vp = price.split(',')
+    vq = qty.split(',')
+    vt = order_type.split(',')
+    vsd = side.split(',')
+
+    num = max(len(vs), len(vp), len(vq), len(vt), len(vsd))
+
+    I = lambda s: s * (num // len(s))
+
+    orders = []
+    for (s, p, q, t, sd) in zip(I(vs), I(vp), I(vq), I(vt), I(vsd)):
+        orders.append(dict(
+            id = uuid32(),
+            time = ts,
+            symbol = s,
+            orderPrice = str(p),
+            orderQty = str(q),
+            orderType = t,
+            side = sd.lower(),
+        ))
+
+    if verbose:
+        print(f"url: {url}")
+        print(f"orders: {orders}")
+
+    headers = make_auth_headers(ts, path, apikey, secret)
+    res = requests.post(url, headers=headers, json=orders)
+
+    data = parse_response(res)
+    print(json.dumps(data, indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    run()
+
